bd2a52741ec94eabb8526de46054789f3b15ceaa
Hinata039 <hnt.hayashi@icloud.com>
2025-06-21 18:04:49 +0900

    Update reservation.py

--- components/reservation.py
+++ components/reservation.py
@@ -2,13 +2,15 @@ import streamlit as st
 from datetime import date
 
 def render():
-    #if st.session_state['login']:
+    if st.session_state['login']:
         st.title("やることリスト")
         st.write("やることリストをまとめることができます。")
 
         #初期化
         if "tasks" not in st.session_state:
             st.session_state.tasks = []
+        if "id_counter" not in st.session_state:
+            st.session_state.id_counter = 0
         
         #タスクの追加フォーム
         with st.form(key = "add_task_form", clear_on_submit=True):
@@ -21,10 +23,32 @@ def render():
             submitted = st.form_submit_button("追加")
 
             if submitted and new_task: #送信され、かつ空白でない時に処理を実行                
-                st.session_state.tasks.append({"title": new_task,"due": str(due), "priority" : priority, "done": False}) #タスクは辞書形式で追加
+                st.session_state.tasks.append({"title": new_task,"due": str(due), "priority" : priority, "done": False, "added_id": st.session_state.id_counter}) #タスクは辞書形式で追加
+                st.session_state.id_counter += 1
                 st.success(f"「{new_task}」を追加しました!")
         
         st.write("## タスク一覧")
+
+        # ソート方法選択
+        sort_method = st.selectbox("ソート方法を選択", options=["追加順", "優先度順", "日付順"])
+
+        priority_order = {"高": 0, "中": 1, "低": 2}
+
+        def sort_tasks(task, method):
+            if method == "追加順":
+                return sorted(task, key=lambda x: x["added_id"])
+            elif method == "優先度順":
+            # 優先度
+                return sorted(task, key=lambda x: x["priority"])
+            elif method == "日付順":
+            # 締切日
+                return sorted(task, key=lambda x: x["due"])
+            else:
+                return task
+        
+        
+        # ソートしたリストを取得
+        sorted_tasks = sort_tasks(st.session_state.tasks, sort_method)
 
         #タスクを表示・更新・削除
         for i, task in enumerate(st.session_state.tasks):
@@ -53,5 +77,5 @@ def render():
                     st.session_state.tasks.pop(i)
                     st.rerun() #削除後にページを更新
                     break         
-    #else:
-        #st.warning("ログインしてください。")
+    else:
+        st.warning("ログインしてください。")
